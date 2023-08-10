bd22814e036d1d89269c69bad56c9c9a1a78024a
SUNGIL KIM <82859578+sonny8569@users.noreply.github.com>
2023-08-10 23:51:29 +0900

    Merge pull request #3
    * viewModel postValue , setValue 변경 sealed 로 Resource data class 대체
    
    * viewModel loading 시 setValue 로변경 , Reposity 들어갈떄만 코루틴 및 LiveData Reso…

--- app/src/main/java/com/sungil/runningproejct_mvvm/object/UserInfo.kt
+++ app/src/main/java/com/sungil/runningproejct_mvvm/object/UserInfo.kt
@@ -1,9 +1,18 @@
 package com.sungil.runningproejct_mvvm.`object`
 
-//Firebase 에 보내고 받을 회원정보 ( 회원가입시 에만)
-data class UserInfo(
-    val id : String = "" ,
-    val password : String = "",
-    val phoneNumber : String = "",
-    val nickName : String = ""
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.sungil.runningproejct_mvvm.appDatabase.AppDatabase
+
+//기본 init 변수 추가
+
+@Entity(
+    tableName = AppDatabase.USERINFO_TABLE
+)
+class UserInfo(
+    @PrimaryKey val id : String = "" ,
+    @ColumnInfo val password : String = "",
+    @ColumnInfo val phoneNumber : String = "",
+    @ColumnInfo val nickName : String = ""
 )
