c37e6aa2950fe33379fdc2d88548d5b995b9774d
user <sonny0204@gmail.com>
2023-08-24 00:50:46 +0900

    팔로워 불러오기 팔로워 개시물 불러오기 비 팔로워 개시물 불러오기 팔로우 기능 추가

--- app/src/main/java/com/sungil/runningproejct_mvvm/main/viewModel/MainViewModel.kt
+++ app/src/main/java/com/sungil/runningproejct_mvvm/main/viewModel/MainViewModel.kt
@@ -4,11 +4,46 @@ import androidx.lifecycle.ViewModel
 import com.sungil.runningproejct_mvvm.repository.MainRepository
 import com.sungil.runningproejct_mvvm.utill.ListenerMessage
 import com.sungil.runningproejct_mvvm.utill.RepositoryListener
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+//MainViewModel inject
+@HiltViewModel
+class MainViewModel @Inject constructor  (private val repository: MainRepository ): ViewModel() , RepositoryListener {
+    //repository
+    var wearLiveData = repository.getRunningRoomDB()
 
-//MainViewModel Factory
-class MainViewModel(private val repository: MainRepository) : ViewModel() , RepositoryListener {
-    val wearLiveData get() = repository.getRunningRoomDB()
-    //repositoryaddasdad
+    var followerLiveData =repository.getFollowerLiveData()
+
+    var followerPostLiveData = repository.getFollowerPostLiveData()
+
+    var unFollowerPostLiveData = repository.getUnFollowerPostLiveData()
+
+    var setFollowerLiveData = repository.getSetFollowerLiveData()
+
+    private var follower = ArrayList<String>()
+    fun getFollower(){
+        val data = repository.getUserInfo()
+        if (data != null) {
+            repository.getFollower(data.id)
+        }
+    }
+
+    fun getFollowerPost() {
+        repository.getFollowerPost(follower)
+    }
+
+    fun getUnFollowerPost() {
+        repository.getUnFollowerPost(follower)
+    }
+
+    fun writeNewFollower(userId : String){
+        val data = repository.getUserInfo() ?: return
+        repository.setNewFollower(userId , data.id)
+    }
+    fun setFollower(followers : ArrayList<String>){
+        this.follower = followers
+    }
+
     override fun onMessageSuccess(data: ListenerMessage) {
         TODO("Not yet implemented")
     }
